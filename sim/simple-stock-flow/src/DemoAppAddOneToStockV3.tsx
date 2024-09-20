3689c88a0404f17f70cbc8fe38ca0f61ba79ecc6
Alexander James Phillips <ajp.global@gmail.com>
2024-09-20 17:11:15 +0100

    Restore interactive StockV3 simulation

--- sim/simple-stock-flow/src/DemoAppAddOneToStockV3.tsx
+++ sim/simple-stock-flow/src/DemoAppAddOneToStockV3.tsx
@@ -15,9 +15,9 @@ export function DemoAppAddOneToStockV3 () {
         const wrapped_model = make_model_stepper({target_refresh_rate: TARGET_REFRESH_RATE})
 
         wrapped_model.add_stock({ wcomponent_id: IDS.stock_a, name: "Stock A", initial: 100 })
-        // wrapped_model.add_stock({ wcomponent_id: IDS.stock_b, name: "Stock B", initial: 10 })
+        wrapped_model.add_stock({ wcomponent_id: IDS.stock_b, name: "Stock B", initial: 10 })
         const action_component__increase_a = wrapped_model.add_variable({ wcomponent_id: IDS.variable__action_increase_a, name: "Action: Increase Stock A", value: 0, is_action: true })
-        // const action_component__move_a_to_b = wrapped_model.add_variable({ wcomponent_id: IDS.variable__action_move_a_to_b, name: "Action: Move A to B", value: 0, is_action: true })
+        const action_component__move_a_to_b = wrapped_model.add_variable({ wcomponent_id: IDS.variable__action_move_a_to_b, name: "Action: Move A to B", value: 0, is_action: true })
         wrapped_model.add_flow({
             wcomponent_id: IDS.flow__flow_into_a,
             name: "Flow into A",
@@ -26,14 +26,14 @@ export function DemoAppAddOneToStockV3 () {
             to_id: IDS.stock_a,
             linked_ids: [IDS.variable__action_increase_a],
         })
-        // wrapped_model.add_flow({
-        //     wcomponent_id: IDS.flow__flow_a_to_b,
-        //     name: "Flow A to B",
-        //     flow_rate: `[${action_component__move_a_to_b.name}]`,
-        //     from_id: IDS.stock_a,
-        //     to_id: IDS.stock_b,
-        //     linked_ids: [IDS.variable__action_move_a_to_b],
-        // })
+        wrapped_model.add_flow({
+            wcomponent_id: IDS.flow__flow_a_to_b,
+            name: "Flow A to B",
+            flow_rate: `[${action_component__move_a_to_b.name}]`,
+            from_id: IDS.stock_a,
+            to_id: IDS.stock_b,
+            linked_ids: [IDS.variable__action_move_a_to_b],
+        })
 
         return wrapped_model
     }, [])
@@ -44,13 +44,10 @@ export function DemoAppAddOneToStockV3 () {
 
     const [current_time, set_current_time] = useState(model_stepper.get_current_time())
     const [stock_a, set_stock_a] = useState(model_stepper.get_latest_state_by_id(IDS.stock_a))
-    // const [stock_b, set_stock_b] = useState(model_stepper.get_latest_state_by_id(IDS.stock_b))
+    const [stock_b, set_stock_b] = useState(model_stepper.get_latest_state_by_id(IDS.stock_b))
     const past_actions_taken = useRef<{step: number, actions_taken: {[action_id: string]: number}}[]>([])
     const actions_taken = useRef<{[action_id: string]: number}>({})
 
-    // This code is present to debug how to use set_value more than once with
-    // simulation.js actions.
-    const tmp_toggle = useRef(false)
 
     useEffect(() => model_stepper.run_simulation({
         target_refresh_rate: TARGET_REFRESH_RATE,
@@ -58,7 +55,7 @@ export function DemoAppAddOneToStockV3 () {
         {
             set_current_time(result.current_time)
             set_stock_a(result.values[IDS.stock_a])
-            // set_stock_b(result.values[IDS.stock_b])
+            set_stock_b(result.values[IDS.stock_b])
 
             const { set_value } = result
             if (!set_value) return
@@ -70,9 +67,7 @@ export function DemoAppAddOneToStockV3 () {
                 Object.keys(last_actions_taken.actions_taken).forEach(action_id =>
                 {
                     const action = model_stepper.get_node_from_id(action_id, true)
-                    // This code is commented out whilst we debug how to use
-                    // set_value more than once with simulation.js actions.
-                    // set_value(action, 0)
+                    set_value(action, 0)
                 })
             }
 
@@ -82,15 +77,7 @@ export function DemoAppAddOneToStockV3 () {
             actions_taken_list.forEach(([action_id, value]) =>
             {
                 const action = model_stepper.get_node_from_id(action_id, true)
-                // set_value(action, value)
-
-                // This code is present to debug how to use set_value more
-                // than once with simulation.js actions.
-                const tmp_value = tmp_toggle.current ? 0 : value
-                console.log("set_value", action._node.id, tmp_value)
-                document.getElementById("debug_output")!.innerHTML += `<br/> set_value of id ${action._node.id} to ${tmp_value}`
-                set_value(action, tmp_value)
-                tmp_toggle.current = !tmp_toggle.current
+                set_value(action, value)
             })
 
             if (actions_taken_list.length)
@@ -128,15 +115,15 @@ export function DemoAppAddOneToStockV3 () {
         <div class="card">
             <div>Current time is {current_time.toFixed(1)}</div>
             <div>Stock A is {round_to_5_dp(stock_a)}</div>
-            {/* <div>Stock B is {round_to_5_dp(stock_b)}</div> */}
+            <div>Stock B is {round_to_5_dp(stock_b)}</div>
 
             <button onClick={action__increase_stock_a}>
                 Increase stock A
             </button>
 
-            {/* <button onClick={action__move_a_to_b}>
+            <button onClick={action__move_a_to_b}>
                 Move A to B
-            </button> */}
+            </button>
         </div>
     </>
 }
