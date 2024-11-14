567f9d0744a48c086addb2d6b4dcad4899ef7a57
Alexander James Phillips <ajp.global@gmail.com>
2024-11-14 23:38:12 +0000

    Update v0.0.1 description

--- sim/uk/london_havering/belhus_woods/src/versions.js
+++ sim/uk/london_havering/belhus_woods/src/versions.js
@@ -6,6 +6,13 @@ export const VERSIONS = [
         datetime: "2024-11-14 20:35 UTC",
         description: `
             Initial version
+
+            * DSM elevation data of the west section of the park.
+            * Basic camera controls with rotate, zoom and tilt, truck and dolly.
+            * Initial attempt to model watersheds, waterflow and flooding from
+            dam construction.
+            * Beaver model with ability to build and move dam position and
+            height.
         `,
     },
 ]
